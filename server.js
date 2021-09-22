97b1eec334bb320d5a746688a7bf161f4035bc4e
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-09-22 14:33:01 +0530

    Make server with express to do tasks in README

--- server.js
+++ server.js
@@ -1,64 +1,58 @@
 // Import modules
-const HTTP = require('http');
+const EXPRESS = require('express');
 const { v4: UUID4 } = require('uuid');
 const FS = require('fs').promises;
 
 
-const SERVER = HTTP.createServer((request, response) => {
-    const STATUS_MATCH = request.url.match(/^\/status\/\d{3}$/g) ?
-        request.url.match(/^\/status\/\d{3}$/g)[0] :
-        null;
-    const DELAY_MATCH = request.url.match(/^\/delay\/\d+$/g) ?
-        request.url.match(/^\/delay\/\d+$/g)[0] :
-        null;
+const APP = EXPRESS();
 
-    switch(request.url) {
-        case '/html' || '/':
-            FS.readFile('./data/index.html', 'utf8')
-                .then((sentBack) => {
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
-                    response.end(sentBack);
-                })
-                .catch((error) => {
-                    console.log(error);
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end('<h1>Error 404: File not found<h1>');
-                });
-            break;
-        case '/json':
-            FS.readFile('./data/response.json', 'utf8')
-                .then((sentBack) => {
-                    response.writeHead(200, { 'Content-Type': 'application/json' });
-                    response.end(sentBack);
-                })
-                .catch((error) => {
-                    console.log(error);
-                    response.writeHead(404, { 'Content-Type': 'text/html' });
-                    response.end('<h1>Error 404: File not found<h1>');
-                });
-            break;
-        case '/uuid':
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify({ uuid: UUID4() }))
-            break;
-        case STATUS_MATCH:
-            response.writeHead(parseInt(request.url.match(/\d{3}$/g)[0]), { 'Content-Type': 'text/html' });
-            response.end(`<h1>Status Code: ${request.url.match(/\d{3}$/g)[0]}<h1>`);
-            break;
-        case DELAY_MATCH:
-            setTimeout(() => {
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.end(`<h1>Server responded after ${request.url.match(/\d+$/g)[0]} seconds<h1>`);
-            }, parseInt(request.url.match(/\d+$/g)[0]) * 1000)
-            break;
-        default:
-            response.writeHead(404, { 'Content-Type': 'text/html' });
-            response.end('<h1>Error 404: File not found<h1>');
+APP.get('/html', (request, response) => {
+    response
+        .status(200)
+        .sendFile(__dirname + '/data/index.html');
+});
+APP.get('/json', (request, response) => {
+    response
+        .status(200)
+        .sendFile(__dirname + '/data/response.json');
+});
+APP.get('/uuid', (request, response) => {
+    response
+        .status(200)
+        .json({ uuid: UUID4() });
+});
+APP.get('/status/:statusCode', (request, response) => {
+    if(request.params.statusCode.match(/^\d{3}$/)) {
+        response
+            .status(parseInt(request.params.statusCode))
+            .send(`<h1>Status Code: ${request.params.statusCode}<h1>`);
+    } else {
+        response
+            .status(404)
+            .send('<h1>Error 404: File not found<h1>');
     }
 });
+APP.get('/delay/:delayTime', (request, response) => {
+    if(request.params.delayTime.match(/^\d+$/)) {
+        setTimeout(() => {
+            response
+                .status(200)
+                .send(`<h1>Server responded after ${request.params.delayTime} seconds<h1>`);
+        }, parseInt(request.params.delayTime) * 1000);
+    } else {
+        response
+            .status(404)
+            .send('<h1>Error 404: File not found<h1>');
+    }
+});
+APP.get('*', (request, response) => {
+    response
+        .status(404)
+        .send('<h1>Error 404: File not found<h1>');
+});
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-SERVER.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+APP.listen(PORT, () => {
+    console.log(`Server started.\nListening on port ${PORT}`);
 });
